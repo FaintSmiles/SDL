db477a907eaaee827e992ec5384fc828bdd58123
FaintSmiles <ifaintsmile@gmail.com>
2016-01-02 04:08:06 -0800

    Update Lights.cpp

--- Lights.cpp
+++ Lights.cpp
@@ -1,71 +1,40 @@
 #include <iostream>
 #include <SDL.h>
-using namespace std;
+#include "Screen.h"
+#include <iomanip>
 
+using namespace std;
+using namespace screeClass;
 //#undef main
 
 int main(int argc, char *args[])
 {
 	//Window Creation Start
-	const int SCREEN_WIDTH = 800;
-	const int SCREEN_HEIGHT = 600;
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		cout << "SDL init failed" << endl;
-		return 1;
+	Screen screen;
+	if (screen.init() == false) {
+		cout << "Error initializing SDL" << endl;
 	}
-	SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-		SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-
-	if (window == NULL) {
-		SDL_Quit();
-		return 2;
-	}
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,SDL_RENDERER_PRESENTVSYNC);
-	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
-		SDL_TEXTUREACCESS_STATIC,SCREEN_WIDTH,SCREEN_HEIGHT);
-	if (renderer == NULL) {
-		cout << "Could not create renderer. " << endl;
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return 3;
-	}
-	if (texture == NULL) {
-		cout << "Could not create texture. " << endl;
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return 4;
-	}
-	//Window Events 
-	Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
-
-	memset(buffer, 0,SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32)); //255 for white or 0xFF = hex
-
-	SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH*sizeof(Uint32));
-	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, texture, NULL, NULL);
-	SDL_RenderPresent(renderer);
 
 	bool quit = false;
-	SDL_Event event;
-	while (!quit) {
+	
+	while (true) {
 		//Update particles
 		//Draw particles
 		//Check for messages/events
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
-				quit = true;
-			}
+
+		screen.setPixel(400, 300, 255, 255,255);
+
+		screen.update();
+
+		if (!screen.processEvents()) {
+			break;
 		}
 	}
 	
 
 
-	delete[] buffer;
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyTexture(texture);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
+	
+	screen.close();
 
 
 	getchar();
